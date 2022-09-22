afd22b6a6cc9e7cb92dc1b0dafd0643f3e1ff59c
Jeff Sahim <jeffreysahim@gmail.com>
2022-09-21 20:11:32 -0400

    Add if

--- friends.js
+++ friends.js
@@ -1,5 +1,6 @@
 var bestFriend = ["Cory Grady", "Bill", "Jeff"]
 
 for (var i = 0; i < bestFriend.length; i++) {
+  if (`This is another great friend ${bestFriend [1]}`)
   console.log (`This is my Best Friend ${bestFriend[i]}`)
 }
